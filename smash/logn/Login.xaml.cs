9b0174b7b936dff4b66567dfe8df1f83009bbf2c
viktorbjerring <viktorbjerring@gmail.com>
2022-03-19 20:13:34 +0100

    added some wpf sadness

---
+++ smash/logn/Login.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace logn
+{
+    /// <summary>
+    /// Interaction logic for UserControl1.xaml
+    /// </summary>
+    public partial class Login : UserControl
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void Login_Button_Click(object sender, RoutedEventArgs e)
+        {
+            var username = Username_TextBox.Text;
+            var password = Password_TextBox.Password;
+            string html = string.Empty;
+            string url = "stefe";
+        }
+    }
+}
---
+++ smash/logn/Register.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace logn
+{
+    /// <summary>
+    /// Interaction logic for Register.xaml
+    /// </summary>
+    public partial class Register : UserControl
+    {
+        public Register()
+        {
+            InitializeComponent();
+        }
+
+        private void Register_Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (Password_Textbox.Password != Confirm_Textbox.Password)
+            {
+                return;
+            }
+            var userEmail = Email_Textbox.Text;
+            var password = Password_Textbox.Password;
+
+        }
+    }
+}
