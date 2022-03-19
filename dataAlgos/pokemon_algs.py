0152a84f24191f879f737c51dddc1c9e26b9a6b4
cakemaster9001 <fisk9889@gmail.com>
2022-03-19 15:08:26 +0100

    Updated the algos to work with updated datastructure

--- dataAlgos/pokemon_algs.py
+++ dataAlgos/pokemon_algs.py
@@ -1,74 +1,58 @@
 
-PokemonKeys = [
-	"Type", 
-	"Height", 
-	"Weight", 
-	"Leveling rate", 
-	"Stats",
-	"Generation",
-	"Pokedex",
-	"Catch rate",
-	"Species",
-	"Gender ratio",
-	"Evolution"]
-
-Pokemon_test = {
-	"Bulbasaur" : {
-		"Leveling rate": "Medium Slow",
-        "Species": "Seed Pokemon",
-        "Catch rate": 45,
-        "Generation": 1,
-        "Gender ratio": 87.5,
-        "Stats": {
-            "HP": 45,
-            "Attack": 49,
-            "Defence": 49,
-            "Sp. Atk": 65,
-            "Sp. Def": 65,
-            "Speed": 45
-        },
-        "Pokedex": "1",
-        "Type": [
-            "Grass",
-            "Poison"
-        ],
-        "Height": 0.7,
-        "Weight": 6.9,
-        "BMI": 14.1,
-        "Evolution": {
-            "Pre": None,
-            "Post": "Ivysaur"
-        },
-	},
-	"Eevee" : {
-		"Type": ["Grass"],
-		"Height": 40,
-		"Weight": 10,
-		"Leveling rate": "Slow",
-		"Stats": {
-			"HP": 100,
-			"Attack": 1,
-			"Defence": 0,
-			"Sp. Atk": 1,
-			"Sp. Def": 2,
-			"Speed": 1000,
-		},
-		"Generation": 0,
-		"Pokedex": 1000,
-		"Catch rate": 60,
-		"Species": "Some group",
-		"Gender ratio": 50,
-		"BMI": 90,
-		"Evolution" : {
-			"Pre": None,
-			"Post": None,
-		}
-	}
-}
+Pokemon_test = [
+    {
+        "id": 1,
+        "name": "Bulbasaur",
+        "pokedex": 1,
+        "leveling_rate": "Medium Slow",
+        "species": "Seed Pokemon",
+        "catch_rate": 45,
+        "generation": 1,
+        "gender_ratio": 87.5,
+        "hp": 45,
+        "attack": 49,
+        "defence": 49,
+        "sp_atk": 65,
+        "sp_def": 65,
+        "speed": 45,
+        "type0": "Grass",
+        "type1": "Poison",
+        "height": 0.7,
+        "weight": 6.9,
+        "bmi": 14.1,
+        "prevolution": None,
+        "evolution": "Ivysaur",
+        "img": "https://img.pokemondb.net/artwork/bulbasaur.jpg"
+    },
+    {
+        "id": 4,
+        "name": "Charmander",
+        "pokedex": 4,
+        "leveling_rate": "Medium Slow",
+        "species": "Lizard Pokemon",
+        "catch_rate": 45,
+        "generation": 1,
+        "gender_ratio": 87.5,
+        "hp": 39,
+        "attack": 52,
+        "defence": 43,
+        "sp_atk": 60,
+        "sp_def": 50,
+        "speed": 65,
+        "type0": "Fire",
+        "type1": "Fire",
+        "height": 0.6,
+        "weight": 8.5,
+        "bmi": 23.6,
+        "prevolution": None,
+        "evolution": "Charmeleon",
+        "img": "https://img.pokemondb.net/artwork/charmander.jpg"
+    }
+]
 
 def getMean(SmashedPokemons, key):
 	mean = 0
-	for i, smashed in enumerate(SmashedPokemons.values()):
+	for smashed in SmashedPokemons:
 		mean += smashed[key]
 
 	return mean / len(SmashedPokemons)
@@ -81,21 +65,16 @@ def getMeanWeight(SmashedPokemons):
 
 def getMeanStats(SmashedPokemons):
 
-	ret = {
-		"HP": 0,
-		"Attack": 0, 
-		"Defence": 0,
-		"Sp. Atk": 0,
-		"Sp. Def": 0,
-		"Speed": 0
-	}
+	stat_keys = ["hp", "attack", "defence", "sp_atk", "sp_def", "speed"]
+	ret = {}
 
-	for i, smashed in enumerate(SmashedPokemons.values()):
-		for key in ret.keys():
-			ret[key] += smashed["Stats"][key]
+	for key in stat_keys:
+		if key not in ret:
+			ret[key] = 0;
 
-	for keys in ret:
-		ret[keys] /= len(SmashedPokemons)
+	for smashed in SmashedPokemons:
+		for key in stat_keys:
+			ret[key] += smashed[key] / len(SmashedPokemons)
 
 	return ret
 
@@ -107,48 +86,49 @@ def getMeanCatchRate(SmashedPokemons):
 
 def countOccurredString(SmashedPokemons, key):
 	ret = {}
-	for i, smashed in enumerate(SmashedPokemons.values()):
+	for smashed in SmashedPokemons:
 
 		if not isinstance(smashed[key], str):
 			raise KeyError(f"Key {key} does not contain a string!")
 
 		occuranceKey = smashed[key]
 		if occuranceKey in ret:
-			ret[occuranceKey] += 1
+			ret[occuranceKey] += 1 / len(SmashedPokemons)
 		else:
-			ret[occuranceKey] = 1
+			ret[occuranceKey] = 1 / len(SmashedPokemons)
 	
 	return ret
 
-def countOccurancesInList(SmashedPokemons, key):
+
+
+def getTypes(SmashedPokemons):
 	ret = {}
-	for i, smashed in enumerate(SmashedPokemons.values()):
-
-		if not isinstance(smashed[key], list):
-			raise KeyError(f"Key {key} is not a list!")
-
-		for item in smashed[key]:
-			if item in ret:
-				ret[item] += 1
+	for smashed in SmashedPokemons:
+		for type in ["type0", "type1"]:
+			if smashed[type] in ret:
+				ret[smashed[type]] += 1 / (2*len(SmashedPokemons))
 			else:
-				ret[item] = 1
-	return ret
+				ret[smashed[type]] = 1 / (2*len(SmashedPokemons))
+	
+
+	return ret;
 
 
 def getProfile(SmashedPokemons):
 
 	return {
-		"Type": countOccurancesInList(SmashedPokemons, "Type"),
-		"Height": getMean(SmashedPokemons, "Height"),
-		"Weight": getMean(SmashedPokemons, "Weight"),
-		"Leveling rate": countOccurredString(SmashedPokemons, "Leveling rate"),
+		"Type": getTypes(SmashedPokemons),
+		"Height": getMean(SmashedPokemons, "height"),
+		"Weight": getMean(SmashedPokemons, "weight"),
+		"Leveling rate": countOccurredString(SmashedPokemons, "leveling_rate"),
 		"Stats": getMeanStats(SmashedPokemons),
-		"Generation": getMean(SmashedPokemons, "Generation"),
-		"Pokedex": ",".join(str(x["Pokedex"]) for x in SmashedPokemons.values()),
-		"Catch rate": getMean(SmashedPokemons, "Catch rate"),
-		"Species": countOccurredString(SmashedPokemons, "Species"),
-		"BMI": getMean(SmashedPokemons, "BMI"),
-		"Gender ratio": getMean(SmashedPokemons, "Gender ratio")
+		"Generation": getMean(SmashedPokemons, "generation"),
+		"Pokedex": ",".join(str(x["pokedex"]) for x in SmashedPokemons),
+		"Catch rate": getMean(SmashedPokemons, "catch_rate"),
+		"Species": countOccurredString(SmashedPokemons, "species"),
+		"BMI": getMean(SmashedPokemons, "bmi"),
+		"Gender ratio": getMean(SmashedPokemons, "gender_ratio")
 	}
-	
-print(f"CountOccured: {getProfile(Pokemon_test)}")
+
+profile = getProfile(Pokemon_test)
+print(f"CountOccured: {profile}")
