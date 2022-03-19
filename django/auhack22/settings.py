b45f197de07144aa0bf14f262f698b5e15bb9866
Morten Haahr Kristensen <mortenhaahrkristensen@gmail.com>
2022-03-19 11:20:58 +0100

    Added support for filtering by pokemon names

--- django/auhack22/settings.py
+++ django/auhack22/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'multiselectfield',
+    'django_filters',
 ]
 
 MIDDLEWARE = [
@@ -133,6 +134,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10
 }
--- django/rest/views.py
+++ django/rest/views.py
@@ -1,15 +1,21 @@
+from django.db.models import Q
+from django_filters import rest_framework as django_filters
+from django_filters.rest_framework import DjangoFilterBackend
+from django.contrib.auth import get_user_model
 from rest_framework import viewsets, permissions
+from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest.serializers import UserSerializer, PokemonSerializer
-from django.contrib.auth import get_user_model
 
 from pokeprofile.models import Pokemon
+
 
 class UserViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows users to be viewed or edited.
     """
-    queryset = get_user_model().objects.all().order_by('-date_joined')
+
+    queryset = get_user_model().objects.all().order_by("-date_joined")
     serializer_class = UserSerializer
     permission_classes = [permissions.IsAuthenticated]
 
@@ -17,7 +23,21 @@ class UserViewSet(viewsets.ModelViewSet):
         obj = super().create(request, *args, **kwargs)
         return obj
 
+class PokemonNameFilter(django_filters.FilterSet):
+    names = django_filters.CharFilter(method="names_comma_seperated")
+
+    def names_comma_seperated(self, queryset, name, value):
+        names = value.split(',')
+        return Pokemon.objects.filter(name__in=names)
+    
+    class Meta:
+        model = Pokemon
+        fields = ['names']
+
+
 class PokemonViewSet(viewsets.ReadOnlyModelViewSet):
-    queryset = Pokemon.objects.all().order_by('pokedex')
+    queryset = Pokemon.objects.all().order_by("pokedex")
     serializer_class = PokemonSerializer
     permission_classes = [permissions.IsAuthenticated]
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = PokemonNameFilter
