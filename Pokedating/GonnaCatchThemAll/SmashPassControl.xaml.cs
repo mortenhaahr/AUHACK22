acd80372237c1ce2c8bad146defe77a44a4cfa1c
viktorbjerring <viktorbjerring@gmail.com>
2022-03-20 10:49:40 +0100

    changes

--- Pokedating/GonnaCatchThemAll/SmashPassControl.xaml.cs
+++ Pokedating/GonnaCatchThemAll/SmashPassControl.xaml.cs
@@ -24,10 +24,13 @@ namespace GonnaCatchThemAll
 
     public partial class SmashPassControl : UserControl
     {
+
+        static SmashPassControl smashPassControl = null;
         private bool animating = false;
         public SmashPassControl()
         {
             InitializeComponent();
+            smashPassControl = this;
         }
 
         public WebAPI.User currentUser;
@@ -48,6 +51,66 @@ namespace GonnaCatchThemAll
         private void DecideFateOfCandidate(bool smash)
         {
             WebAPI.WebClient.PostSmash(currentUser.id, candidates[0].id, smash);
+            candidates.RemoveAt(0);
+        }
+
+        private void Button_Smash_Click(object sender, RoutedEventArgs e)
+        {
+            animating = true;
+            Image_Pokeball.Visibility = Visibility.Visible;
+            RotateTransform rotate = new RotateTransform();
+            TranslateTransform position = new TranslateTransform();
+            ScaleTransform scale = new ScaleTransform();
+            TransformGroup transform = new TransformGroup();
+            DoubleAnimation upAnimation = new DoubleAnimation(0, -canvas.ActualHeight * 0.4, TimeSpan.FromSeconds(1.2));
+            DoubleAnimation spinAnimation = new DoubleAnimation(0, 360 * 3, TimeSpan.FromSeconds(1.2));
+            transform.Children.Add(position);
+            transform.Children.Add(rotate);
+            transform.Children.Add(scale);
+            Image_Pokeball.RenderTransform = transform;
+            position.BeginAnimation(TranslateTransform.YProperty, upAnimation);
+            rotate.BeginAnimation(RotateTransform.CenterYProperty, upAnimation);
+            rotate.BeginAnimation(RotateTransform.AngleProperty, spinAnimation);
+            Timer t = new Timer();
+            t.Interval = 1200;
+            t.Elapsed += new ElapsedEventHandler((object source, ElapsedEventArgs e) =>
+            {
+                smashPassControl.Image_Profile.Dispatcher.BeginInvoke(new Action(() =>
+                {
+                    t.Stop();
+                    t = new Timer();
+                    t.Interval = 1200;
+                    Point relativePoint = Image_Pokeball.TransformToAncestor(canvas).Transform(new Point(0, 0));
+                    ScaleTransform scale = new ScaleTransform();
+                    RotateTransform rotate = new RotateTransform();
+                    TransformGroup transform = new TransformGroup();
+                    transform.Children.Add(scale);
+                    transform.Children.Add(rotate);
+                    smashPassControl.Image_Profile.RenderTransform = transform;
+                    scale.CenterX = relativePoint.X + Image_Pokeball.ActualWidth / 2;
+                    scale.CenterY = relativePoint.Y - Image_Pokeball.ActualHeight / 2;
+                    rotate.CenterX = relativePoint.X + Image_Pokeball.ActualWidth / 2;
+                    rotate.CenterY = relativePoint.Y - Image_Pokeball.ActualHeight / 2;
+                    DoubleAnimation scaleAnimation = new DoubleAnimation(1, 0, TimeSpan.FromSeconds(1));
+                    DoubleAnimation rotateAnimation = new DoubleAnimation(0, 360 * 7, TimeSpan.FromSeconds(1));
+                    scale.BeginAnimation(ScaleTransform.ScaleXProperty, scaleAnimation);
+                    scale.BeginAnimation(ScaleTransform.ScaleYProperty, scaleAnimation);
+                    rotate.BeginAnimation(RotateTransform.AngleProperty, rotateAnimation);
+                    t.Elapsed += new ElapsedEventHandler((object source, ElapsedEventArgs e) =>
+                    {
+                animating = false;
+                DecideFateOfCandidate(true);
+                        t.Stop();
+                    });
+                    t.Start();
+                }));
+            });
+            t.Start();
+
+        }
+
+        private void Button_Pass_Click(object sender, RoutedEventArgs e)
+        {
             animating = true;
             ScaleTransform scale = new ScaleTransform();
             DoubleAnimation scaleAnimation = new DoubleAnimation(1, 0, TimeSpan.FromSeconds(1.2));
@@ -65,38 +128,6 @@ namespace GonnaCatchThemAll
                 t.Stop();
             });
             t.Start();
-            candidates.RemoveAt(0);
-        }
-
-        private void Button_Smash_Click(object sender, RoutedEventArgs e)
-        {
-            animating = true;
-            Image_Pokeball.Visibility = Visibility.Visible;
-            RotateTransform rotate = new RotateTransform();
-            TranslateTransform position = new TranslateTransform();
-            TransformGroup transform = new TransformGroup();
-            DoubleAnimation upAnimation = new DoubleAnimation(0, -canvas.ActualHeight * 0.4, TimeSpan.FromSeconds(1.2));
-            DoubleAnimation spinAnimation = new DoubleAnimation(0, 360 * 3, TimeSpan.FromSeconds(1.2));
-            transform.Children.Add(position);
-            transform.Children.Add(rotate);
-            Image_Pokeball.RenderTransform = transform;
-            position.BeginAnimation(TranslateTransform.YProperty, upAnimation);
-            rotate.BeginAnimation(RotateTransform.CenterYProperty, upAnimation);
-            rotate.BeginAnimation(RotateTransform.AngleProperty, spinAnimation);
-            Timer t = new Timer();
-            t.Interval = 1200;
-            t.Elapsed += new ElapsedEventHandler((object source, ElapsedEventArgs e) =>
-            {
-                animating = false;
-                t.Stop();
-                DecideFateOfCandidate(true);
-            });
-            t.Start();
-
-        }
-
-        private void Button_Pass_Click(object sender, RoutedEventArgs e)
-        {
             DecideFateOfCandidate(false);
         }
 
