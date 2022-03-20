4ed8377acde688f923995fe2680b4c87de0d3c56
Anton Vigen Smolarz <anton@smolarz.dk>
2022-03-20 10:51:45 +0100

    Upload working

--- Pokedating/GonnaCatchThemAll/Profile.xaml.cs
+++ Pokedating/GonnaCatchThemAll/Profile.xaml.cs
@@ -151,22 +151,53 @@ namespace GonnaCatchThemAll
             user.first_name = FirstName_TextBox.Text;
             user.last_name = LastName_TextBox.Text;
             user.age = int.Parse(Age_TextBox.Text);
-            user.gender = int.Parse(Gender_ComboBox.Text);
+            switch (Gender_ComboBox.Text)
+            {
+                case "Other":
+                    {
+                        user.gender = 0;
+                    }
+                    break;
+                case "Male":
+                    {
+                        user.gender = 1;
+                    }
+                    break;
+                case "Female":
+                    {
+                        user.gender = 2;
+                    }
+                    break;
+            }
             user.description = Bio_Textbox.Text;
             user.age_from = (int)Math.Round(ageSlider.SelectionStart);
             user.age_to = (int)Math.Round(ageSlider.SelectionEnd);
             user.search_radius = (int)Math.Round(distSlider.SelectionEnd);
-            user.photo0 = "Blank"; //Image0.Source.ToString();
-            user.photo1 = "Blank"; //Image1.Source.ToString();
-            user.photo2 = "Blank"; //Image2.Source.ToString();
-            user.photo3 = "Blank"; //Image3.Source.ToString();
-            user.photo4 = "Blank"; //Image4.Source.ToString();
-            user.photo5 = "Blank"; //Image5.Source.ToString();
-            user.photo6 = "Blank"; //Image6.Source.ToString();
-            user.photo7 = "Blank"; //Image7.Source.ToString();
-            user.photo8 = "Blank"; //Image8.Source.ToString();
+            user.photo0 = null; //Image0.Source.ToString();
+            user.photo1 = null; //Image1.Source.ToString();
+            user.photo2 = null; //Image2.Source.ToString();
+            user.photo3 = null; //Image3.Source.ToString();
+            user.photo4 = null; //Image4.Source.ToString();
+            user.photo5 = null; //Image5.Source.ToString();
+            user.photo6 = null; //Image6.Source.ToString();
+            user.photo7 = null; //Image7.Source.ToString();
+            user.photo8 = null; //Image8.Source.ToString();
             user.last_seen_lat = 56.171089;
             user.last_seen_long = 10.189372;
+            List<int> lookFor = new List<int>();
+            if (CheckBoxOther.IsChecked == true)
+            {
+                lookFor.Add(0);
+            }
+            if (CheckBoxMale.IsChecked == true)
+            {
+                lookFor.Add(1);
+            }
+            if (CheckBoxFemale.IsChecked == true)
+            {
+                lookFor.Add(2);
+            }
+            user.looking_for = lookFor.ToArray();
             WebAPI.WebClient.Post<WebAPI.User>("users/", user);
         }
 
